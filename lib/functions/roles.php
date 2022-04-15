27a0444dc7baff899d6c31a493dcf79086548e92
Rob Knight <raknight@ucsc.edu>
2022-04-15 16:59:01 -0700

    fix: 🐛 Update slug and label; Set proper capabilities. Resolves #2

--- lib/functions/roles.php
+++ lib/functions/roles.php
@@ -17,25 +17,24 @@
  *
  * @return void
  */
-function ucsc_custom_functionality_site_admin_role() {
+function ucsc_custom_functionality_site_manager_role() {
 
 	$admin_role = get_role( 'administrator' );
 
 	// Array of capabilities you do not want the new role to have.
 	$remove_caps = array(
-		'switch_themes'    => 0,
-		'edit_themes'      => 0,
-		'activate_plugins' => 0,
-		'edit_plugins'     => 0,
-		'manage_options'   => 0,
+		'switch_themes'             => 0,
+		'edit_themes'               => 0,
+        'edit_theme_options'        => 0,
+		'edit_plugins'              => 0
 	);
 
 	// Run a diff on the admin role capabilities and the removed rules.
 	$new_role_caps = array_diff_key( $admin_role->capabilities, $remove_caps );
 
 	// Add the role.
-	$new_role = add_role( 'Site Administrator', 'Site Administrator', $new_role_caps );
+	$new_role = add_role( 'ucsc_site_manager', 'UCSC Site Manager', $new_role_caps );
 
 }
 
-add_action( 'after_setup_theme', 'ucsc_custom_functionality_site_admin_role' );
+add_action( 'after_setup_theme', 'ucsc_custom_functionality_site_manager_role' );
