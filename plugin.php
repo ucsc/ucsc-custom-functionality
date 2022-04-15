b0e78f92bf023fc56f83f14cb8580f349b2e8ab7
Rob Knight <raknight@ucsc.edu>
2022-04-15 13:38:05 -0700

    Consistently name things

--- plugin.php
+++ plugin.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name: UCSC Main Core Functionality Plugin
- * Plugin URI: https://github.com/ucsc/ucsc-main-core-functionality-plugin.git
- * Description: Contains custom functionality for UCSC Websites. Theme independent.
- * Version: 0.0.0--beta
+ * Plugin Name: UCSC Custom Functionality
+ * Plugin URI: https://github.com/ucsc/ucsc-custom-functionality.git
+ * Description: Contains custom functionality for UCSC WordPress Websites.
+ * Version: 0.1.0
  * Author: UC Santa Cruz
  * Author URI: https://github.com/ucsc
  * License: GPL2
  *
- * @package SC_Custom_Functionality
+ * @package ucsc
+ *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
  * that you can use any other version of the GPL.
@@ -33,7 +34,7 @@
  * @return array request arguments
  */
 
-function sc_custom_functionality_hidden( $r, $url ) {
+function ucsc_custom_functionality_hidden( $r, $url ) {
 	if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) ) {
 		return $r; // Not a plugin update request. Bail immediately.
 	}
@@ -43,13 +44,13 @@ function sc_custom_functionality_hidden( $r, $url ) {
 	$r['body']['plugins'] = serialize( $plugins );
 	return $r;
 }
-add_filter( 'http_request_args', 'sc_custom_functionality_hidden', 5, 2 );
+add_filter( 'http_request_args', 'ucsc_custom_functionality_hidden', 5, 2 );
 
 // Plugin Directory.
-define( 'SC_DIR', dirname( __FILE__ ) );
+define( 'UCSC_DIR', dirname( __FILE__ ) );
 
 // Include Customization files.
 // Roles.
-if ( file_exists( SC_DIR . '/lib/functions/roles.php' ) ) {
-			include_once SC_DIR . '/lib/functions/roles.php';
+if ( file_exists( UCSC_DIR . '/lib/functions/roles.php' ) ) {
+	include_once UCSC_DIR . '/lib/functions/roles.php';
 }
