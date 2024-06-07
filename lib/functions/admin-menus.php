ba4f8b4470feb4e4b87efded7392e91cd7de7c06
Jason Chafin <Herm71@users.noreply.github.com>
2024-06-07 10:11:59 -0700

    feat: ensure the plugin properly checks for functions before calling them
    Fixes #21

--- lib/functions/admin-menus.php
+++ lib/functions/admin-menus.php
@@ -11,61 +11,71 @@
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 
-/**
- * Remove Link Manager from Dash Sidebar
- *
- * @return void
- * Removes deprecated link manager from dashboard sidebar
- * @package
- * @since
- * @author UC Santa Cruz
- * @license GNU General Public License 2.0+
- */
-function ucsc_custom_functionality_customize_dash() {
+if ( ! function_exists( 'ucsc_custom_functionality_customize_dash' ) ) {
+	/**
+	 * Remove Link Manager from Dash Sidebar
+	 *
+	 * @return void
+	 * Removes deprecated link manager from dashboard sidebar
+	 * @package
+	 * @since
+	 * @author UC Santa Cruz
+	 * @license GNU General Public License 2.0+
+	 */
+
+
+	function ucsc_custom_functionality_customize_dash() {
 
 		remove_menu_page( 'link-manager.php' );
 
+	}
 }
 add_action( 'admin_menu', 'ucsc_custom_functionality_customize_dash' );
 
-/**
- * Remove items from Admin Bar
- *
- * @param mixed $wp_admin_bar
- * @return void
- * Removes unwanted items from the WP Admin bar
- * @package
- * @since
- * @author UC Santa Cruz
- * @license GNU General Public License 2.0+
- */
-function ucsc_custom_functionality_remove_from_admin_bar( $wp_admin_bar ) {
-	if ( ! current_user_can( 'edit_themes' ) ) {
-		$wp_admin_bar->remove_node( 'customize' );
+
+if ( ! function_exists( 'ucsc_custom_functionality_remove_from_admin_bar' ) ) {
+
+	/**
+	* Remove items from Admin Bar
+	*
+	* @param mixed $wp_admin_bar
+	* @return void
+	* Removes unwanted items from the WP Admin bar
+	* @package
+	* @since
+	* @author UC Santa Cruz
+	* @license GNU General Public License 2.0+
+	*/
+	function ucsc_custom_functionality_remove_from_admin_bar( $wp_admin_bar ) {
+		if ( ! current_user_can( 'edit_themes' ) ) {
+			$wp_admin_bar->remove_node( 'customize' );
+		}
 	}
 }
 add_action( 'admin_bar_menu', 'ucsc_custom_functionality_remove_from_admin_bar', 999 );
 
+if ( ! function_exists( 'ucsc_custom_functionality_remove_from_dash_panel' ) ) {
+	/**
+	* Remove items from Dashboard panel
+	*
+	* @return void
+	* Remove left-hand dashboard items
+	* @package
+	* @since
+	* @author UC Santa Cruz
+	* @license GNU General Public License 2.0+
+	*/
 
-/**
- * Remove items from Dashboard panel
- *
- * @return void
- * Remove left-hand dashboard items, currently only removes Customize
- * @package
- * @since
- * @author UC Santa Cruz
- * @license GNU General Public License 2.0+
- */
-function ucsc_custom_functionality_remove_from_dash_panel() {
-	global $submenu;
-	foreach ( $submenu as $name => $items ) {
-		if ( $name === 'themes.php' ) {
-			foreach ( $items as $i => $data ) {
-				if ( in_array( 'customize', $data, true ) ) {
-					unset( $submenu[ $name ][ $i ] );
+	function ucsc_custom_functionality_remove_from_dash_panel() {
+		global $submenu;
+		foreach ( $submenu as $name => $items ) {
+			if ( $name === 'themes.php' ) {
+				foreach ( $items as $i => $data ) {
+					if ( in_array( 'customize', $data, true ) ) {
+						unset( $submenu[ $name ][ $i ] );
 
-					return;
+						return;
+					}
 				}
 			}
 		}
--- plugin.php
+++ plugin.php
@@ -11,32 +11,35 @@
  * @package ucsc-custom-functionality
  */
 
-/**
- * Don't Update Plugin
- *
- * @since 1.0.0
- *
- * This prevents you being prompted to update if there's a public plugin
- * with the same name.
- *
- * @author Mark Jaquith
- * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
- *
- * @param array  $r, request arguments
- * @param string $url, request url
- * @return array request arguments
- */
+if ( ! function_exists( 'ucsc_custom_functionality_hidden' ) ) {
+	/**
+	* Don't Update Plugin
+	*
+	* @since 1.0.0
+	*
+	* This prevents you being prompted to update if there's a public plugin
+	* with the same name.
+	*
+	* @author Mark Jaquith
+	* @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
+	*
+	* @param array  $r, request arguments
+	* @param string $url, request url
+	* @return array request arguments
+	*/
 
-function ucsc_custom_functionality_hidden( $r, $url ) {
-	if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) ) {
-		return $r; // Not a plugin update request. Bail immediately.
+	function ucsc_custom_functionality_hidden( $r, $url ) {
+		if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) ) {
+			return $r; // Not a plugin update request. Bail immediately.
+		}
+		$plugins = unserialize( $r['body']['plugins'] );
+		unset( $plugins->plugins[ plugin_basename( __FILE__ ) ] );
+		unset( $plugins->active[ array_search( plugin_basename( __FILE__ ), $plugins->active ) ] );
+		$r['body']['plugins'] = serialize( $plugins );
+		return $r;
 	}
-	$plugins = unserialize( $r['body']['plugins'] );
-	unset( $plugins->plugins[ plugin_basename( __FILE__ ) ] );
-	unset( $plugins->active[ array_search( plugin_basename( __FILE__ ), $plugins->active ) ] );
-	$r['body']['plugins'] = serialize( $plugins );
-	return $r;
 }
+
 add_filter( 'http_request_args', 'ucsc_custom_functionality_hidden', 5, 2 );
 
 // Set plugin directory.
