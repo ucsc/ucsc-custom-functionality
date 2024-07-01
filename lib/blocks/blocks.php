94397c052a8eaf9997c16541cd3e4c496352dd73
Jason Chafin <jchafin@ucsc.edu>
2024-07-01 07:52:33 -0700

    Add ID and Class attributes to ACF block

--- lib/blocks/blocks.php
+++ lib/blocks/blocks.php
@@ -8,7 +8,18 @@ function ucsc_register_custom_blocks(){
 }
 
 function ucsc_article_source_block( $block ) {
-
+	$id = 'article-source-' . $block['id'];
+	if( !empty($block['anchor']) ) {
+		$id = $block['anchor'];
+	}
+	// Create Class attribute allowing custom "className" and "align" values
+	$className = 'article-source';
+	if( !empty($block['className']) ) {
+		$className .= ' ' . $block['className'];
+	}
+	if(!empty($block['align']) ) {
+		$className .= ' align' . $block['align'];
+	}
 	// Get the post ID
 	$post_id = get_the_ID();
 
@@ -16,5 +27,6 @@ function ucsc_article_source_block( $block ) {
 	$article_source = get_field('article_source', $post_id);
 
 	// Display the article source
-	echo '<p>' . $article_source . '</p>';
+	?>
+	<p id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>"><?php echo $article_source; ?></p><?php
 }
