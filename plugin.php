6888effba71c41a0398f3e9311d918f1ed0d0f2d
Jason Chafin <jchafin@ucsc.edu>
2024-07-10 09:41:00 -0700

    Add `Update URI:` to `plugin.php` header metadata #27

--- plugin.php
+++ plugin.php
@@ -6,41 +6,11 @@
  * Version: 1.6.0
  * Author: UC Santa Cruz
  * Author URI: https://github.com/ucsc
+ * Update URI: https://github.com/ucsc
  * License: GPL2
  *
  * @package ucsc-custom-functionality
  */
-
-if ( ! function_exists( 'ucsc_custom_functionality_hidden' ) ) {
-	/**
-	* Don't Update Plugin
-	*
-	* @since 1.0.0
-	*
-	* This prevents you being prompted to update if there's a public plugin
-	* with the same name.
-	*
-	* @author Mark Jaquith
-	* @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
-	*
-	* @param array  $r, request arguments
-	* @param string $url, request url
-	* @return array request arguments
-	*/
-
-	function ucsc_custom_functionality_hidden( $r, $url ) {
-		if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) ) {
-			return $r; // Not a plugin update request. Bail immediately.
-		}
-		$plugins = unserialize( $r['body']['plugins'] );
-		unset( $plugins->plugins[ plugin_basename( __FILE__ ) ] );
-		unset( $plugins->active[ array_search( plugin_basename( __FILE__ ), $plugins->active ) ] );
-		$r['body']['plugins'] = serialize( $plugins );
-		return $r;
-	}
-}
-
-add_filter( 'http_request_args', 'ucsc_custom_functionality_hidden', 5, 2 );
 
 // Set plugin directory.
 define( 'UCSC_DIR', dirname( __FILE__ ) );
