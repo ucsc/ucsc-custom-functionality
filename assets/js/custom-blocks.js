052b60988bf89dbb9888e9262cf14ad50df77815
Vinny Listrani <vinny@tri.be>
2024-11-18 15:11:37 -0600

    Fixes build script. Adds correct taxonomy switcher script.

--- assets/js/custom-blocks.js
+++ assets/js/custom-blocks.js
@@ -1,20 +1,26 @@
-(function ($) {
+( function ( $ ) {
 	$( document ).ready( function () {
 		if ( typeof acf === 'undefined' ) {
 			return;
 		}
 
-		acf.add_filter('select2_ajax_data', function ( data, args, $input, field, instance ) {
-			if ( ! data.field_key || data.field_key !== 'taxonomy_list_items' ) {
+		acf.add_filter(
+			'select2_ajax_data',
+			function ( data, args, $input, field, instance ) {
+				if (
+					! data.field_key ||
+					! data.field_key.includes( 'taxonomy_list_items' )
+				) {
+					return data;
+				}
+
+				// Add selected taxonomy type to request for the current block
+				data.taxonomy_selected = acf
+					.getField( $( 'div[data-name=taxonomies_list]' ) )
+					.val();
+
 				return data;
 			}
-
-			// Add selected taxonomy type to request for the current block
-			data.taxonomy_selected = acf.getField( 'taxonomies_list' ).val();
-
-			return data;
-
-		});
-	});
-
-})( jQuery );
+		);
+	} );
+} )( jQuery );
