66b21951740113b64f545606530eb97c19a16d3e
Brett Cooper <brett@thewiznerd.com>
2024-11-19 14:07:47 -0700

    Added styles for photo of the week archive.

--- src/Post_Types/Photo_Of_The_Week/Photo_Of_The_Week.php
+++ src/Post_Types/Photo_Of_The_Week/Photo_Of_The_Week.php
@@ -5,30 +5,30 @@ namespace UCSC\Blocks\Post_Types\Photo_Of_The_Week;
 use UCSC\Blocks\Post_Types\Post_Types;
 
 class Photo_Of_The_Week extends Post_Types {
-	
+
 	public const NAME = 'photo_of_the_week';
 
 	public function get_args(): array {
-		return [
+		return array(
 			'labels'          => $this->get_labels(),
 			'query_var'       => false,
 			'map_meta_cap'    => true,
-			'supports'        => [
+			'supports'        => array(
 				'title',
-			],
+			),
 			'show_in_menu'    => true,
 			'public'          => true,
 			'capability_type' => 'post',
 			'show_in_rest'    => true,
-			'has_archive'     => 'photos-of-the-week',
-			'rewrite'         => [
+			'has_archive'     => true,
+			'rewrite'         => array(
 				'slug' => 'photo-of-the-week',
-			],
-		];
+			),
+		);
 	}
-	
+
 	protected function get_labels(): array {
-		return [
+		return array(
 			'name'         => esc_html__( 'Photo Of The Week', 'ucsc' ),
 			'menu_name'    => esc_html__( 'Photo Of The Week', 'ucsc' ),
 			'add_new'      => esc_html__( 'Add Photo', 'ucsc' ),
@@ -38,7 +38,6 @@ class Photo_Of_The_Week extends Post_Types {
 			'all_items'    => esc_html__( 'Photos Of The Week', 'ucsc' ),
 			'view_item'    => esc_html__( 'View Photo Of The Week', 'ucsc' ),
 			'search_items' => esc_html__( 'Search Photos Of The Week', 'ucsc' ),
-		];
+		);
 	}
-
 }
