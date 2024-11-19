e97d723887c420782804a003b88283b2338dc92a
Brett Cooper <brett@thewiznerd.com>
2024-11-19 15:15:02 -0700

    Simplified svgs.

--- src/views/photos_week_loop/index.php
+++ src/views/photos_week_loop/index.php
@@ -22,19 +22,17 @@ $posts = $c->get_query( $paged );
 						<?php $author = get_field( Photo_Of_The_Week_Meta::PHOTOGRAPHER, get_the_ID() ); ?>
 						<?php if ( ! empty( $author ) ) : ?>
 						<p class="photo-of-the-week__author">
-							<svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<g stroke="currentColor" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round">
-									<path d="M2.5.5h4M9.5 12.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-									<path d="M14.5 15.5h-13a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1Z"/>
-								</g>
+							<svg width="16" height="16" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+								<path d="M2.5.5h4M9.5 12.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+								<path d="M14.5 15.5h-13a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1Z"/>
 							</svg>
 							<?php echo $author; ?>
 						</p>
 						<?php endif; ?>
 					</div>
 					<a class="photo-of-the-week__download-link" href="<?php echo get_the_permalink( get_the_ID() ); ?>" target="_blank">
-						<svg width="16" height="16" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-							<path d="M8 .5v11M3.5 7 8 11.5 12.5 7M.5 15.5h15" stroke-linecap="round" stroke-linejoin="round"/>
+						<svg width="16" height="16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+							<path d="M8 .5v11M3.5 7 8 11.5 12.5 7M.5 15.5h15" />
 						</svg>
 						<?php echo esc_html__( 'Download Image', 'ucsc' ); ?>
 					</a>
