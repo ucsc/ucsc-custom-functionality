1c55ec6443b30ab514a67286dd98b4add0b3c930
Brett Cooper <brett@thewiznerd.com>
2024-12-03 15:14:47 -0700

    Working on header.

--- src/Components/Post_Header_Block_Controller.php
+++ src/Components/Post_Header_Block_Controller.php
@@ -6,29 +6,55 @@ use UCSC\Blocks\Blocks\Post_Header_Block;
 use UCSC\Blocks\Components\Traits\With_Primary_Term;
 
 class Post_Header_Block_Controller {
-    
-    use With_Primary_Term;
-    
-    protected array $block;
+	
+	use With_Primary_Term;
+	
+	protected array $block;
 
-    public function __construct( $block ) {
-        $this->block = (array) $block;
-    }
-    
-    public function get_layout() {
-        $layout = get_field( Post_Header_Block::LAYOUT ) ?? Post_Header_Block::LAYOUT_BIG;
-        
-        return $layout;
-    }
-    
-    public function get_primary_category(): ?string {
-        $category = $this->get_primary_term( get_the_ID() );
-        
-        if ( empty( $category ) ) {
-            return null;
-        }
-        
-        return (string) $category->name;
-    }
-    
+	public function __construct( $block ) {
+		$this->block = (array) $block;
+	}
+
+	public function get_attributes(): string {
+		return wp_kses_data( get_block_wrapper_attributes([
+			'class' => implode(' ', [
+				'ucsc-post-header-block',
+				'alignfull',
+				'has-ucsc-primary-blue-background-color',
+				'has-white-color',
+				'has-global-padding',
+				'is-layout-constrained',
+			]),
+		]) );
+	}
+	
+	public function get_layout() {
+		return get_field( Post_Header_Block::LAYOUT ) ?? Post_Header_Block::LAYOUT_BIG;
+	}
+	
+	public function get_primary_category(): ?string {
+		$category = $this->get_primary_term( get_the_ID() );
+		
+		if ( empty( $category ) ) {
+			return null;
+		}
+		
+		return (string) $category->name;
+	}
+
+	public function get_image(): array {
+		$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+		if ( $thumbnail_id === false ) {
+			return [];
+		}
+
+		$thumbnail = get_post( $thumbnail_id );
+
+		return [
+			'image'       => get_the_post_thumbnail( get_the_ID() ),
+			'description' => $thumbnail->post_excerpt,
+			'attribution' => $thumbnail->post_content,
+		];
+	}
+	
 }
--- src/views/post_header_block/index.php
+++ src/views/post_header_block/index.php
@@ -7,25 +7,51 @@ use UCSC\Blocks\Components\Post_Header_Block_Controller;
  */
 $c            = new Post_Header_Block_Controller( $block );
 $primary_term = $c->get_primary_category();
+$image        = $c->get_image();
 ?>
-<section <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<a href="<?php echo get_bloginfo( 'url' ); ?>">
-		<?php echo esc_html__( 'Home', 'ucsc' ); ?>
-	</a>
-	<?php if ( ! empty( $primary_term ) ) : ?>
-		<span>
-			<?php echo $primary_term ?>
-		</span>
-	<?php endif; ?>
-	<h1>
-		<?php echo get_the_title( get_the_ID() ); ?>
-	</h1>
-	<div>
-		<?php echo get_the_excerpt( get_the_ID() ); ?>
-	</div>
-	<?php echo get_the_date( 'F j, Y', get_the_ID() ); ?>
-	<?php echo do_blocks( '<!-- wp:post-author-name /-->' ); ?>
-	<?php if ( has_post_thumbnail( get_the_ID() ) ) : ?>
-		<?php echo get_the_post_thumbnail( get_the_ID() ); ?>
+<section <?php echo $c->get_attributes(); ?>>
+	<header class="ucsc-post-header-block__header">
+		<nav class="ucsc-post-header-block__breadcrumb">
+			<a href="<?php echo get_bloginfo( 'url' ); ?>">
+				<?php echo esc_html__( 'Home', 'ucsc' ); ?>
+			</a>
+		</nav>
+
+		<hgroup>
+			<?php if ( ! empty( $primary_term ) ) : ?>
+				<p class="ucsc-post-header-block__eyebrow">
+					<?php echo $primary_term ?>
+				</span>
+			<?php endif; ?>
+			<h1>
+				<?php echo get_the_title( get_the_ID() ); ?>
+			</h1>
+		</hgroup>
+
+		<p class="ucsc-post-header-block__excerpt">
+			<?php echo get_the_excerpt( get_the_ID() ); ?>
+		</p>
+
+		<div class="ucsc-post-header-block__meta">
+			<?php echo get_the_date( 'F j, Y', get_the_ID() ); ?>
+			<?php echo do_blocks( '<!-- wp:post-author-name /-->' ); ?>
+		</div>
+	</header>
+
+	<?php if ( $image ) : ?>
+	<figure class="ucsc-post-header-block__image">
+		<?php echo $image['image']; ?>
+
+		<?php if ( $image['description'] || $image['attribution'] ) : ?>
+		<figcaption>
+			<?php if ( $image['description'] ) : ?>
+				<p><?php echo esc_html( $image['description'] ); ?></p>
+			<?php endif; ?>
+			<?php if ( $image['attribution'] ) : ?>
+				<p><?php echo esc_html( $image['attribution'] ); ?></p>
+			<?php endif; ?>
+		</figcaption>
+		<?php endif; ?>
+	</figure>
 	<?php endif; ?>
 </section>
