984289b6d2ca09905bdec0e263e70b8be77b981c
Mykhailo Los <mlkilderkin@gmail.com>
2024-12-04 10:48:06 +0200

    Fix block issue with cache

--- src/Core.php
+++ src/Core.php
@@ -12,10 +12,31 @@ class Core {
 		$this->blocks();
 		$this->scripts();
 	}
+
+    /**
+     * @param array          $block      The block attributes.
+     * @param string         $content    The block content.
+     * @param bool           $is_preview Whether the block is being rendered for editing preview.
+     * @param int            $post_id    The current post being edited or viewed.
+     * @param \WP_Block|null $wp_block   The block instance (since WP 5.5).
+     */
+    public function render_template( array $block, string $content = '', bool $is_preview = false, int $post_id = 0, ?\WP_Block $wp_block = null ): void {
+        $template = $block['render_template'];
+        $path     = str_replace( 'build/views/', 'src/views/', $block['path'] );
+
+        if ( ! file_exists( "$path/$template" ) ) {
+            return;
+        }
+
+        include "$path/$template"; // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.NotAbsolutePath
+    }
 	
 	protected function blocks(): void {
-		add_action( 'init', static function (): void {
-			register_block_type( UCSC_DIR . '/src/views/news_block' );
+		add_action( 'init', function (): void {
+            $args = [
+                'render_callback' => [ $this, 'render_template' ],
+            ];
+            register_block_type_from_metadata( trailingslashit( UCSC_DIR . '/build/views/news_block' ) . '/block.json', $args );
 			( new News_Block() )->init();
 			( new News_Blocks_Hooks() )->hooks();
 			( new Assets_Subscriber() )->register();
