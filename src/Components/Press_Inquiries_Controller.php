9875e126fd0ed53ea3d6e44ef2f6220b5fa2195c
Brett Cooper <brett@thewiznerd.com>
2024-12-04 14:39:08 -0700

    Added scripting.

--- src/Components/Press_Inquiries_Controller.php
+++ src/Components/Press_Inquiries_Controller.php
@@ -7,19 +7,36 @@ use UCSC\Blocks\Blocks\Press_Inquiries_Block;
 class Press_Inquiries_Controller {
 
 	protected array $block;
+	protected string $panel_id;
 
 	public function __construct( $block ) {
-		$this->block = (array) $block;
+		$this->block    = (array) $block;
+		$this->panel_id = wp_unique_id( 'press-inquiries-block-' );
+	}
+
+	public function get_attributes(): string {
+		return wp_kses_data( get_block_wrapper_attributes([
+			'class' => implode( ' ', [
+				'ucsc-press-inquiries-block',
+				'alignfull',
+				'is-layout-constrained',
+				'has-global-padding',
+			] ),
+		]) );
 	}
 	
 	public function get_press_contacts(): array {
 		$contacts = get_field( Press_Inquiries_Block::PRESS_INQUIRIES );
-        
-        if ( empty( $contacts ) ) {
-            return [];
-        }
-        
-        return $contacts;
+		
+		if ( empty( $contacts ) ) {
+			return [];
+		}
+		
+		return $contacts;
+	}
+
+	public function get_panel_id(): string {
+		return esc_attr( $this->panel_id );
 	}
 	
 	public function get_media_text(): string {
--- src/views/press_inquiries_block/index.php
+++ src/views/press_inquiries_block/index.php
@@ -15,58 +15,66 @@ $media_file  = $c->get_media_file();
 $is_empty = empty( $contacts ) && empty( $media_image ) && empty( $media_file ) && empty( $media_text );
 
 if ( is_admin() && $is_empty ) {
-    echo esc_html__( 'This block will display post press inquiries section.', 'ucsc' );
+	echo esc_html__( 'This block will display post press inquiries section.', 'ucsc' );
 
-    return;
+	return;
 }
 
 if ( $is_empty ) {
 	return;
 }
 ?>
-<section <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<p><?php echo esc_html__( 'Press Inquiries', 'ucsc' ); ?></p>
-	<?php if ( ! empty( $contacts ) ) : ?>
+<section <?php echo $c->get_attributes(); ?>>
 	<div>
-		<h3><?php echo esc_html__( 'Press Contact', 'ucsc' ); ?></h3>
-		<?php foreach ( $contacts as $contact ) : ?>
-			<?php if ( empty( $contact ) ) : ?>
-				<?php continue; ?>
-			<?php endif; ?>
-			<h5><?php echo $contact[ Press_Inquiries_Block::PRESS_NAME ]; ?></h5>
-			<p>
-				<a href="mailto:<?php echo $contact[ Press_Inquiries_Block::PRESS_EMAIL ]; ?>">
-					<?php echo $contact[ Press_Inquiries_Block::PRESS_EMAIL ]; ?>
-				</a>
-			</p>
-			<p>
-				<a href="tel:<?php echo $contact[ Press_Inquiries_Block::PRESS_PHONE ]; ?>">
-					<?php echo $contact[ Press_Inquiries_Block::PRESS_PHONE ]; ?>
-				</a>
-			</p>
-		<?php endforeach; ?>
+		<button class="ucsc-press-inquiries-block__toggle" type="button" aria-expanded="false" aria-controls="<?php echo $c->get_panel_id(); ?>">
+			<?php echo esc_html__( 'Press Inquiries', 'ucsc' ); ?>
+		</button>
 	</div>
-	<?php endif; ?>
-	<?php if ( ! empty( $media_text ) || ! empty( $media_file ) || ! empty( $media_image ) ) : ?>
-		<div>
-			<h3><?php echo esc_html__( 'Media Access', 'ucsc' ); ?></h3>
-			<?php if ( ! empty( $media_file ) ) : ?>
-			<p>
-				<a href="<?php echo $media_file ?>" target="_blank">
-					<?php echo esc_html__( 'Access Paper', 'ucsc' ); ?>
-				</a>
-			</p>
+	<div class="ucsc-press-inquiries-block__container" id="<?php echo $c->get_panel_id(); ?>" inert>
+		<div class="ucsc-press-inquiries-block__content">
+			<?php if ( ! empty( $contacts ) ) : ?>
+			<div>
+				<h3><?php echo esc_html__( 'Press Contact', 'ucsc' ); ?></h3>
+				<?php foreach ( $contacts as $contact ) : ?>
+					<?php if ( empty( $contact ) ) : ?>
+						<?php continue; ?>
+					<?php endif; ?>
+					<h5><?php echo $contact[ Press_Inquiries_Block::PRESS_NAME ]; ?></h5>
+					<p>
+						<a href="mailto:<?php echo $contact[ Press_Inquiries_Block::PRESS_EMAIL ]; ?>">
+							<?php echo $contact[ Press_Inquiries_Block::PRESS_EMAIL ]; ?>
+						</a>
+					</p>
+					<p>
+						<a href="tel:<?php echo $contact[ Press_Inquiries_Block::PRESS_PHONE ]; ?>">
+							<?php echo $contact[ Press_Inquiries_Block::PRESS_PHONE ]; ?>
+						</a>
+					</p>
+				<?php endforeach; ?>
+			</div>
 			<?php endif; ?>
-			<?php if ( ! empty( $media_image ) ) : ?>
-				<p>
-					<a href="<?php echo $media_image ?>" target="_blank">
-						<?php echo esc_html__( 'Image Download', 'ucsc' ); ?>
-					</a>
-				</p>
-			<?php endif; ?>
-			<?php if ( ! empty( $media_text ) ) : ?>
-				<?php echo $media_text ?>
+			<?php if ( ! empty( $media_text ) || ! empty( $media_file ) || ! empty( $media_image ) ) : ?>
+				<div>
+					<h3><?php echo esc_html__( 'Media Access', 'ucsc' ); ?></h3>
+					<?php if ( ! empty( $media_file ) ) : ?>
+					<p>
+						<a href="<?php echo $media_file ?>" target="_blank">
+							<?php echo esc_html__( 'Access Paper', 'ucsc' ); ?>
+						</a>
+					</p>
+					<?php endif; ?>
+					<?php if ( ! empty( $media_image ) ) : ?>
+						<p>
+							<a href="<?php echo $media_image ?>" target="_blank">
+								<?php echo esc_html__( 'Image Download', 'ucsc' ); ?>
+							</a>
+						</p>
+					<?php endif; ?>
+					<?php if ( ! empty( $media_text ) ) : ?>
+						<?php echo $media_text ?>
+					<?php endif; ?>
+				</div>
 			<?php endif; ?>
 		</div>
-	<?php endif; ?>
+	</div>
 </section>
