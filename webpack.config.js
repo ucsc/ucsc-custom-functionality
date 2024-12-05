428a37b1cd941eb776d0df76743f3d10b71763c9
Brett Cooper <brett@thewiznerd.com>
2024-12-05 08:20:57 -0700

    Added custom block styles.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config.js');
+
+module.exports = {
+	...defaultConfig,
+	...{
+		entry: {
+			'css/post-terms': path.resolve(process.cwd(), 'assets/scss/blocks', 'post-terms.scss'),
+			'css/social-sharing': path.resolve(process.cwd(), 'assets/scss/blocks', 'social-sharing.scss'),
+		},
+	},
+};
