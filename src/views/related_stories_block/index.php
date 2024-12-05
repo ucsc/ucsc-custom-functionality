3b7998ba5299389205ecced532e947841e2fb6e2
Brett Cooper <brett@thewiznerd.com>
2024-12-05 11:12:02 -0700

    Updated icons and block names.

--- src/views/related_stories_block/index.php
+++ src/views/related_stories_block/index.php
@@ -13,7 +13,7 @@ if ( empty( $items ) && is_admin() ) {
 	?>
 	<section <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 		<h3 class="ucsc-related-stories-block__header-title">
-			<?php echo esc_html__( 'Please configure the Related Stories Block in order to populate content', 'ucsc' ); ?>
+			<?php echo esc_html__( 'Please configure the Related Stories block in order to populate content', 'ucsc' ); ?>
 		</h3>
 	</section>
 	<?php
