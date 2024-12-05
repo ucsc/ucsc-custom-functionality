3f6daffbe8ea6f1617a840b2f66ee57d3ac1b258
Brett Cooper <brett@thewiznerd.com>
2024-12-05 08:45:06 -0700

    Got block styles enqueuing working.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	...defaultConfig,
 	...{
 		entry: {
+			...defaultConfig.entry(),
 			'css/post-terms': path.resolve(process.cwd(), 'assets/scss/blocks', 'post-terms.scss'),
 			'css/social-sharing': path.resolve(process.cwd(), 'assets/scss/blocks', 'social-sharing.scss'),
 		},
