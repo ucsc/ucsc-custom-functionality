acd6ce45052355e5a33c0fb05019dae23bc4fdf6
Brett Cooper <brett@thewiznerd.com>
2024-12-05 07:23:16 -0700

    Added block filters for categories and social share.

--- src/Template/Template_Subscriber.php
+++ src/Template/Template_Subscriber.php
@@ -10,6 +10,8 @@ class Template_Subscriber {
 		add_filter( 'render_block', static function ( $block_content = '', $block = [] ) use ( $block_renderer ) {
 			$block_content = $block_renderer->adjust_author_block( $block_content, $block );
 			$block_content = $block_renderer->adjust_featured_image_block( $block_content, $block );
+			$block_content = $block_renderer->adjust_post_terms_block( $block_content, $block );
+			$block_content = $block_renderer->adjust_social_share_block( $block_content, $block );
 			
 			return $block_content;
 		}, 100, 2 );
