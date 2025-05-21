71f88636404e3f69868b428bff95ae132c90edee
Rob Knight <raknight@ucsc.edu>
2025-05-21 09:52:56 -0700

    Fix oEmbed issues by removing old code (#85)
    Fixes #83
    
    * style: ♻️ Spacing
    * fix: 🐛 Remove old code that disables plugin update checking. This code broke oEmbeds

--- plugin.php
+++ plugin.php
@@ -1,5 +1,4 @@
 <?php declare(strict_types=1);
-
 /**
  * Plugin Name: UCSC Custom Functionality
  * Plugin URI: https://github.com/ucsc/ucsc-custom-functionality.git
@@ -11,40 +10,6 @@
  *
  * @package ucsc-custom-functionality
  */
-
-if ( ! function_exists( 'ucsc_custom_functionality_hidden' ) ) {
-	/**
-	* Don't Update Plugin
-	*
-	* @since 1.0.0
-	*
-	* This prevents you being prompted to update if there's a public plugin
-	* with the same name.
-	*
-	* @author Mark Jaquith
-	 *
-	 * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
-	*
-	* @param array  $r, request arguments
-	* @param string $url, request url
-	 *
-	 * @return array request arguments
-	*/
-
-	function ucsc_custom_functionality_hidden( array $r, string $url ): array {
-		if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) ) {
-			return $r; // Not a plugin update request. Bail immediately.
-		}
-		$plugins = unserialize( $r['body']['plugins'] );
-		unset( $plugins->plugins[ plugin_basename( __FILE__ ) ] );
-		unset( $plugins->active[ array_search( plugin_basename( __FILE__ ), $plugins->active ) ] );
-		$r['body']['plugins'] = serialize( $plugins );
-
-		return $r;
-	}
-}
-
-add_filter( 'http_request_args', 'ucsc_custom_functionality_hidden', 5, 2 );
 
 // Set plugin directory.
 define( 'UCSC_DIR', dirname( __FILE__ ) );
