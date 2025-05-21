79c1297b70fab8aa241d0b43a56acf41b64522fe
Rob Knight <raknight@ucsc.edu>
2025-05-21 09:29:44 -0700

    style: ♻️ Spacing

--- lib/functions/scripts.php
+++ lib/functions/scripts.php
@@ -15,6 +15,7 @@
 if ( file_exists( UCSC_DIR . '/lib/functions/scripts/disable-xmlrpc.php' ) ) {
 	include_once UCSC_DIR . '/lib/functions/scripts/disable-xmlrpc.php';
 }
+
 // Add Google TagManager.
 if ( file_exists( UCSC_DIR . '/lib/functions/scripts/ga.php' ) ) {
 	include_once UCSC_DIR . '/lib/functions/scripts/ga.php';
