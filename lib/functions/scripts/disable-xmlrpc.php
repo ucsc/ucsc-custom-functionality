54ee38d6e4e910e69ddb7d903bc2b943f24e82db
Rob Knight <raknight@ucsc.edu>
2026-04-13 11:56:43 -0700

    fix: 🔒️ Remove option to enable XML-RPC, since it is a security risk (#95)
    * fix: 🔒️ Remove option to enable XML-RPC, since it is a security risk
    * docs: 📝 Remove documentation about XML-RPC toggle

--- lib/functions/scripts/disable-xmlrpc.php
+++ lib/functions/scripts/disable-xmlrpc.php
@@ -7,20 +7,17 @@
  *
  * see: https://pantheon.io/docs/wordpress-best-practices#avoid-xml-rpc-attacks
  *
- * XMLRPC is disabled by default. Site administrators can re-enable it from
- * Settings → UCSC Custom Functionality.
+ * XMLRPC is always disabled when this plugin is active.
  */
 
-if ( ! (bool) get_option( 'ucsc_enable_xmlrpc', false ) ) {
-	add_filter(
-		'xmlrpc_methods',
-		function () {
-			return array();
-		},
-		PHP_INT_MAX
-	);
+add_filter(
+	'xmlrpc_methods',
+	function () {
+		return array();
+	},
+	PHP_INT_MAX
+);
 
-	// Removes the link from the <head>.
-	// Avoids a11y issue with broken link
-	remove_action( 'wp_head', 'rsd_link' );
-}
+// Removes the link from the <head>.
+// Avoids a11y issue with broken link
+remove_action( 'wp_head', 'rsd_link' );
